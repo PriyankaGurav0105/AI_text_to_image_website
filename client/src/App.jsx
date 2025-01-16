6996d7c0a415752626541dc21e92e376ee684ae9
PriyankaGurav0105 <priyankagurav0105@gmail.com>
2025-01-16 17:28:43 +0530

    `feat: updated App component with NavBar and AppContextProvider, refactored routes and imports`

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,4 @@
+import NavBar from "./components/NavBar"
 import BuyCredit from "./pages/BUyCredit"
 import Home from "./pages/Home"
 import Result from "./pages/Result"
@@ -5,7 +6,8 @@ import { Routes, Route } from "react-router-dom"
 
 const App = () => {
   return (
-    <div>
+    <div className='px-4 sm:px-10 md:px-14 lg:px-28 min-h-screen bg-gradient-to-b from-slate-200 to-slate-300'>
+      <NavBar/>
       <Routes>
       <Route path="/" element={<Home/>}/>
       <Route path="/result" element={<Result/>}/>
