fae1a37fb1dc3bb28135eec458667ae93fd6c8e7
PriyankaGurav0105 <priyankagurav0105@gmail.com>
2025-01-16 16:05:16 +0530

    React-Router-dom added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,18 @@
+import BuyCredit from "./pages/BUyCredit"
+import Home from "./pages/Home"
+import Result from "./pages/Result"
+import { Routes, Route } from "react-router-dom"
 
 const App = () => {
   return (
     <div>
+      <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/result" element={<Result/>}/>
+      <Route path="/buy" element={<BuyCredit/>}/>
       
+
+      </Routes>
     </div>
   )
 }
---
+++ client/src/pages/Result.jsx
@@ -0,0 +1,10 @@
+
+const Result = () => {
+  return (
+    <div>
+      Results
+    </div>
+  )
+}
+
+export default Result
