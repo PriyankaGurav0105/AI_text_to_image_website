d5cc8ca7a0a180e0b961a0f7f7bfbb471e580245
PriyankaGurav0105 <priyankagurav0105@gmail.com>
2025-01-19 17:21:34 +0530

    Display credit star and name of the user with logout feature

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -8,7 +8,7 @@ import { AppContext } from '../context/AppContext.jsx';
 const NavBar = () => {
 
 const navigate = useNavigate();
-const {user, setShowLogin}= useContext(AppContext);
+const {user, setShowLogin, logout, credit}= useContext(AppContext);
 
   return (
     <div className="flex items-center justify-between py-4">
@@ -21,14 +21,14 @@ const {user, setShowLogin}= useContext(AppContext);
         <div className='flex items-center gap-2 sm:gap-3'>
             <button onClick={()=>navigate('/buy')} className=' flex items-center gap-2 bg-blue-200 px-4 sm:px-6 py-1.5 sm:py-3 rounded-full hover:scale-105 transition-all duration-700'>
                 <img src={assets.credit_star} className='w-5'/>
-                <p className='text-xs sm:text-sm font-medium text-gray-600 '>Credits left : 50</p>
+                <p className='text-xs sm:text-sm font-medium text-gray-600 '>Credits left : {credit}</p>
             </button>
-            <p className='text-gray-600 max-sm:hidden pl-4'>Hi, Priyanka Gurav</p>
+            <p className='text-gray-600 max-sm:hidden pl-4'>Hi, {user.name}</p>
             <div className='relative group'>
                 <img src={assets.profile_icon} className='w-10 drop-shadow'/>
                 <div className='hidden absolute group-hover:block top-0 right-0 z-10 text-black rounded pt-12'>
                     <ul className='list-none m-0 p-2 bg-white rounded-md border text-sm'>
-                    <li className='py-1 px-2 cursor-pointer pr-10'>Logout</li>
+                    <li onClick={logout} className='py-1 px-2 cursor-pointer pr-10'>Logout</li>
                     </ul></div>
             </div>
         </div>  
