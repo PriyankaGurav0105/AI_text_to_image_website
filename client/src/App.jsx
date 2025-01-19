f8ef18409da75346c28b5297d46aeb73d4b6e70a
PriyankaGurav0105 <priyankagurav0105@gmail.com>
2025-01-19 21:54:09 +0530

    added keys

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -15,7 +15,7 @@ const App = () => {
   const {showLogin} = useContext(AppContext);
   return (
     <div className='px-4 sm:px-10 md:px-14 lg:px-28 min-h-screen bg-gradient-to-b from-slate-200 to-slate-300'>
-      <ToastContainer position="bottom-right"/>
+      <ToastContainer position="top-right"/>
       <NavBar/>
       {showLogin && <Login  />}
       <Routes>
